dda51103c1c54e9fa615a8ca746b189c87b510df
SpencasaurusRex <SpencasaurusRex@gmail.com>
2021-04-18 05:39:31 -0400

    Update to 2020.3.4f1

--- Assets/Scripts/CodePanel.cs
+++ Assets/Scripts/CodePanel.cs
@@ -13,22 +13,21 @@ public class CodePanel : MonoBehaviour {
 
     void Start() {
         text = GetComponentInChildren<Text>();
-        foreach (var command in robot.Commands) {
-            text.text += command.ToText() + "\n";
-        }
 
         GameController.Instance.OnTick2 += Tick;
         targetHighlight = HighlightPanel.anchoredPosition;
     }
 
-    void ParseCode() {
-
-    }
-
     int i = 0;
 
     void Tick() {
-        i = robot.CommandIndex;
+        if (robot && robot.Commands != null) {
+            foreach (var command in robot.Commands) {
+                text.text += command.ToText() + "\n";
+            }
+            i = robot.CommandIndex;
+        }
+        
         targetHighlight = new Vector2(targetHighlight.x, LineOffset * i);
     }
 
--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 public class Robot : MonoBehaviour {        
     // Show in Inspector
     [TextArea]
-    public string Code;
+    string Code;
     
     // Runtime
     GridObject gridObject;
